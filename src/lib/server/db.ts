3c6d3370c821fa6e9b499b0546e1cd2d4c6f9ece
HoangLuc123 <123654261+HoangLuc123@users.noreply.github.com>
2026-08-02 16:24:13 +0700

    fix: add SSL and port for Aiven

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,14 +1,34 @@
+// import 'server-only';
+// import mysql from 'mysql2/promise';
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || '',
+//   database: process.env.DB_NAME || 'tech_store',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// export default pool;
+
+
 import 'server-only';
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'tech_store',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 22199,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false, // Bắt buộc với Aiven
+  },
 });
 
 export default pool;
